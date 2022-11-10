0b2cf5414d0ba7822aab6e50d4c674e9260f2479
Francesco C <fran461j@outlook.it>
2022-11-10 18:40:45 +0100

    image

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,6 @@ var app = new Vue({
     el: '#root',
     data: {
         message : 'Hello boys',
+        source : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
     }
 });
